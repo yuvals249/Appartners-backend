913bbf3337aacf79fa704833c760e628a550d6ae
Yuval <yuvals249@gmail.com>
2025-03-29 15:58:17 +0300

    Start to work on user preferences features model, still does not works well

--- users/models/user_preferences_features.py
+++ users/models/user_preferences_features.py
@@ -6,6 +6,12 @@ from apartments.models import Feature
 
 
 class UserPreferencesFeatures(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.AutoField(primary_key=True)  # Auto-incrementing ID field
     user_preferences = models.ForeignKey(UserPreferences, on_delete=models.CASCADE, related_name="user_preference_features")
-    feature = models.ForeignKey(Feature, on_delete=models.CASCADE, related_name="feature_users")
+    # feature = models.ForeignKey(Feature, on_delete=models.CASCADE, related_name="feature_user_preferences")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user_preferences.id} - {self.feature.name}"
