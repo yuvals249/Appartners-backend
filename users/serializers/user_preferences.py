27e6325d0d2e5c8d863007103c362483b06c8c12
Yuval <yuvals249@gmail.com>
2025-03-29 15:55:53 +0300

    Update user preferences serializer

--- users/serializers/user_preferences.py
+++ users/serializers/user_preferences.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
-from users.models.user_preferences import UserPreferences
+from ..models import UserPreferences
 
 
-class UserPreferencesSerializer(serializers.ModelSerializer):
+class UserPreferencesGetSerializer(serializers.ModelSerializer):
+    city = serializers.CharField(source="city.name", read_only=True)  # Return the city name
+    preferred_move_in_date = serializers.DateField(source="move_in_date")  # Use move_in_date directly
+    # features = serializers.CharField(source="features.name", read_only=True)  # Return the city name
+    price_range = serializers.SerializerMethodField()
+
     class Meta:
         model = UserPreferences
-        fields = ('city', 'min_price', 'max_price', 'move_in_date', 'number_of_roommates')
+        fields = [
+            'city',
+            'preferred_move_in_date',
+            # 'features',
+            'number_of_roommates',
+            'price_range',
+        ]
+
+    def get_price_range(self, obj):
+        return {
+            "min": obj.min_price,
+            "max": obj.max_price
+        }
