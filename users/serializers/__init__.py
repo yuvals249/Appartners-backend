78857c8a2be356b9361dc9aebfaa7cdd6eaa1c71
Yuval <yuvals249@gmail.com>
2025-03-29 15:50:42 +0300

    Renamed files name

--- users/serializers/__init__.py
+++ users/serializers/__init__.py
@@ -1,7 +1,7 @@
 from .questionnaire import QuestionnaireSerializer
 from .user_details import UserDetailsSerializer
-from .user_preferences import UserPreferencesSerializer
+from .user_preferences import UserPreferencesGetSerializer
 from .user_preferences_features import UserPreferencesFeaturesSerializer
 
-__all__ = ["QuestionnaireSerializer", "UserDetailsSerializer", "UserPreferencesSerializer",
+__all__ = ["QuestionnaireSerializer", "UserDetailsSerializer", "UserPreferencesGetSerializer",
            "UserPreferencesFeaturesSerializer"]
