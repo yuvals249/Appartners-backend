23f6d3edd8611c76caeb0b83da668db71a1573f1
Yuval <yuvals249@gmail.com>
2025-03-31 10:18:35 +0300

    Add console logging

--- appartners/settings.py
+++ appartners/settings.py
@@ -131,3 +131,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
