8ca47a36b8b9260483547ef8fde2b25817b257ea
Tom Sheinman <tomsheinman@Toms-MacBook-Pro.local>
2025-04-03 12:48:20 +0300

    fixed migration error

--- users/migrations/0007_alter_userdetails_id.py
+++ users/migrations/0007_alter_userdetails_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0006_auto_20250331_2115'),
+        ('users', '0001_initial'),
     ]
 
     operations = [
